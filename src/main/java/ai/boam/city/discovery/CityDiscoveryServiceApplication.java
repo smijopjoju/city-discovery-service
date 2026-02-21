a895024f9d4ebf23dc940ba06c912497e2fa01fd
smijopjoju <sj.2.3.4u@gmail.com>
2026-02-21 10:31:52 +0100

    chore: initial project structure from spring initializr

---
+++ src/main/java/ai/boam/city/discovery/CityDiscoveryServiceApplication.java
@@ -0,0 +1,13 @@
+package ai.boam.city.discovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CityDiscoveryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CityDiscoveryServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ai/boam/city/discovery/CityDiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ai.boam.city.discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CityDiscoveryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
